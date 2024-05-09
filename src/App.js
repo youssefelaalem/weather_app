8b182eb097d67d1631094370baa3570988a6c5ac
youssefelaalem <youssefelaalem82@gmail.com>
2024-05-09 16:27:43 +0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+
+import WeatherApplication from './Components/WeatherApp/WeatherApplication.jsx';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <WeatherApplication/>
+    </div>
+  );
+}
+
+export default App;
